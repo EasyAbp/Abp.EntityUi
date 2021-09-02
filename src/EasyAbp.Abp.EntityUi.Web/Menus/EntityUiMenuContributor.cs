0f72ea0c56385311178b5ef773d24c0599a49217
gdlcf88 <gdlcf88@outlook.com>
2021-09-02 14:47:45 +0800

    Skip menu items with nonexistent permissions. Fix #1.

--- src/EasyAbp.Abp.EntityUi.Web/Menus/EntityUiMenuContributor.cs
+++ src/EasyAbp.Abp.EntityUi.Web/Menus/EntityUiMenuContributor.cs
@@ -9,17 +9,32 @@ using EasyAbp.Abp.EntityUi.Modules.Dtos;
 using EasyAbp.Abp.EntityUi.Web.Localization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
-using Volo.Abp.Application.Dtos;
+using Microsoft.Extensions.Logging;
+using Volo.Abp.Authorization;
+using Volo.Abp.Security.Claims;
 using Volo.Abp.UI.Navigation;
 
 namespace EasyAbp.Abp.EntityUi.Web.Menus
 {
     public class EntityUiMenuContributor : IMenuContributor
     {
+        private ILogger<EntityUiMenuContributor> _logger;
+        private IAbpAuthorizationPolicyProvider _policyProvider;
+        private ICurrentPrincipalAccessor _currentPrincipalAccessor;
+        private IIntegrationAppService _integrationAppService;
+        private IEntityUiStringLocalizerProvider _stringLocalizerProvider;
+
         private Dictionary<string, IStringLocalizer> ModuleNameStringLocalizers { get; } = new();
         
         public async Task ConfigureMenuAsync(MenuConfigurationContext context)
         {
+            var loggerFactory = context.ServiceProvider.GetRequiredService<ILoggerFactory>();
+            _logger = loggerFactory.CreateLogger<EntityUiMenuContributor>();
+            _policyProvider = context.ServiceProvider.GetRequiredService<IAbpAuthorizationPolicyProvider>();
+            _currentPrincipalAccessor = context.ServiceProvider.GetRequiredService<ICurrentPrincipalAccessor>();
+            _integrationAppService = context.ServiceProvider.GetRequiredService<IIntegrationAppService>();
+            _stringLocalizerProvider = context.ServiceProvider.GetRequiredService<IEntityUiStringLocalizerProvider>();
+            
             if (context.Menu.Name == StandardMenus.Main)
             {
                 await ConfigureMainMenuAsync(context);
@@ -28,46 +43,64 @@ namespace EasyAbp.Abp.EntityUi.Web.Menus
 
         protected virtual async Task ConfigureMainMenuAsync(MenuConfigurationContext context)
         {
-            var integrationAppService = context.ServiceProvider.GetRequiredService<IIntegrationAppService>();
-            var stringLocalizerProvider = context.ServiceProvider.GetRequiredService<IEntityUiStringLocalizerProvider>();
+            var integration = await _integrationAppService.GetAsync();
 
-            var integration = await integrationAppService.GetAsync();
-
-            await AddMenuItemsAsync(context.Menu, integration.MenuItems, integration, stringLocalizerProvider, context);
+            await AddMenuItemsAsync(context.Menu, integration.MenuItems, integration, context);
         }
 
         protected virtual async Task AddMenuItemsAsync(IHasMenuItems parent, IEnumerable<MenuItemDto> menuItems,
-            EntityUiIntegrationDto integration, IEntityUiStringLocalizerProvider stringLocalizerProvider,
-            MenuConfigurationContext context)
+            EntityUiIntegrationDto integration, MenuConfigurationContext context)
         {
             foreach (var menuItem in menuItems)
             {
-                if (menuItem.Permission != null && !await context.IsGrantedAsync(menuItem.Permission))
+                if (menuItem.Permission != null && !await IsFoundAndGrantedAsync(menuItem.Permission, context))
                 {
                     continue;
                 }
-                
+
                 var l = await GetOrCreateStringLocalizerAsync(
-                    integration.Modules.First(x => x.Name == menuItem.ModuleName), stringLocalizerProvider);
+                    integration.Modules.First(x => x.Name == menuItem.ModuleName), _stringLocalizerProvider);
 
                 var child = new ApplicationMenuItem(menuItem.Name, l[menuItem.LocalizationItemName]);
-                
+
                 if (menuItem.MenuItems.IsNullOrEmpty())
                 {
                     if (menuItem.ParentName.IsNullOrEmpty())
                     {
                         continue;
                     }
-                    
+
                     child.Url = $"/EntityUi/{menuItem.ModuleName}/{menuItem.EntityName}";
                 }
                 else
                 {
-                    await AddMenuItemsAsync(child, menuItem.MenuItems, integration, stringLocalizerProvider, context);
+                    await AddMenuItemsAsync(child, menuItem.MenuItems, integration, context);
                 }
-                
+
                 parent.Items.Add(child);
             }
+        }
+
+        protected virtual async Task<bool> IsFoundAndGrantedAsync(string policyName, MenuConfigurationContext context)
+        {
+            if (policyName == null)
+            {
+                throw new ArgumentNullException(nameof(policyName));
+            }
+
+            var policy = await _policyProvider.GetPolicyAsync(policyName);
+            
+            if (policy == null)
+            {
+                _logger.LogWarning($"[Entity UI] No policy found: {policyName}.");
+                
+                return false;
+            }
+            
+            return (await context.AuthorizationService.AuthorizeAsync(
+                _currentPrincipalAccessor.Principal,
+                null,
+                policyName)).Succeeded;
         }
 
         protected virtual async Task<IStringLocalizer> GetOrCreateStringLocalizerAsync(ModuleDto module,
