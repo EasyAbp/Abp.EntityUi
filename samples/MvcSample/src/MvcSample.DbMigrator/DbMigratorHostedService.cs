87dd4580a26758642c54fa98675d913cc9c1557d
gdlcf88 <gdlcf88@outlook.com>
2023-12-28 00:47:30 +0800

    Fix `DbMigratorHostedService`

--- samples/MvcSample/src/MvcSample.DbMigrator/DbMigratorHostedService.cs
+++ samples/MvcSample/src/MvcSample.DbMigrator/DbMigratorHostedService.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using MvcSample.Data;
 using Serilog;
 using Volo.Abp;
+using Volo.Abp.Data;
 
 namespace MvcSample.DbMigrator
 {
@@ -23,11 +24,12 @@ namespace MvcSample.DbMigrator
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             using (var application = await AbpApplicationFactory.CreateAsync<MvcSampleDbMigratorModule>(options =>
-                   {
-                       options.Services.ReplaceConfiguration(_configuration);
-                       options.UseAutofac();
-                       options.Services.AddLogging(c => c.AddSerilog());
-                   }))
+            {
+                options.Services.ReplaceConfiguration(_configuration);
+                options.UseAutofac();
+                options.Services.AddLogging(c => c.AddSerilog());
+                options.AddDataMigrationEnvironment();
+            }))
             {
                 await application.InitializeAsync();
 
